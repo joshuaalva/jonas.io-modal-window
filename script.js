ffb0ff7f833929735200f49e6ae4901883a528a0
Joshua Alvarado <joshalva3@gmail.com>
2023-07-10 23:54:12 -0500

    using functions inside listeners and working with classes

--- script.js
+++ script.js
@@ -8,14 +8,28 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 // console.log(btnsOpenModal);
 
+const openModal = function () {
+  modal.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener(`click`, function () {
-    // console.log(`Button clicked`);
-    modal.classList.remove(`hidden`);
-    // can pass in more with commas
-    // hidden class is now gone
-  });
+  btnsOpenModal[i].addEventListener(`click`, openModal);
 }
+// console.log(`Button clicked`);
+// modal.classList.remove(`hidden`);
+// overlay.classList.remove(`hidden`);
+// modal.style.display = `block`;
+// can pass in more with commas
+// hidden class is now gone
+
+const closedModal = function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+
+btnCloseModal.addEventListener(`click`, closedModal);
+overlay.addEventListener(`click`, closedModal);
 // this is now an element
 // multiple elements with the same class
 // one line of code doesn't need the curly braces
